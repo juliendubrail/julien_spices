ee6e2db11f72680e9cf3a753f6373491a8bad9c9
juliendubrail <juliendubrail@users.noreply.github.com>
2016-09-07 13:17:59 +0200

    Create superfun.js

---
+++ wk2/superfun.js
@@ -0,0 +1,27 @@
+
+var num; 
+
+var doublenum;
+
+num = 750;
+
+function timestwo(numb){
+return numb = numb * 2;
+}
+
+doublenum = timestwo(num);
+
+var numbers;
+
+numbers[0]=num;
+numbers[1]=doublenum;
+
+for (var i =0; i < number.length; i++){
+numbers[i| = timestwo(numbers[i]);
+}
+
+numbers = {};
+
+numbers.num = doublenum;
+
+
